fc7cb3f1cddca019f4b9f7cbd3fcf9f62831dc79
boboslz <61566910+boboslz@users.noreply.github.com>
2020-04-21 14:34:31 -0300

    Add files via upload

---
+++ Passar de pagina/src/pagina2.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+
+<form action="App.svelte" method="get">
+    <input type="submit" value="VOLTAR" 
+         name="Submit" id="frm1_submit" />
+</form>
---
+++ pagina1.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<form action="pagina2.svelte" method="get">
+    <input type="submit" value="Ir para a proxima página" 
+         name="Submit" id="frm1_submit" />
+</form>
